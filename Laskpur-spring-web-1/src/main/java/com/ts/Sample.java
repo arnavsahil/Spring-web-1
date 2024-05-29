ad4f37a3c91faa3d8858840109770550e66666b7
arnavsahil <121164493+arnavsahil@users.noreply.github.com>
2024-05-30 00:24:51 +0530

    Add files via upload

---
+++ Laskpur-spring-web-1/src/main/java/com/ts/Sample.java
@@ -0,0 +1,26 @@
+package com.ts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //-> @Controller + @ResponseBody
+public class Sample {
+
+	@GetMapping("/vinod") //---> localhost:8080/app/vinod (API)
+	public String test(){
+		System.out.println("WE ARE IN DOGET METHOD...");
+		return "WE ARE IN DOGET METHOD...";
+	}
+	
+	@PostMapping("/don")
+	public String test1(){
+		System.out.println("Sir kuch samjh nahi aa rha...");
+		return "Sir kuch samjh nahi aa rha...";
+	}
+
+}
+
+
